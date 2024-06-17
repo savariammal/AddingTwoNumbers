6dd2ffb7e06039793cf40ab624d1faae61e23835
savariammal <160878030+savariammal@users.noreply.github.com>
2024-06-17 09:47:06 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,9 @@
+
+var a=+prompt("Enter A value:");
+document.write(" Value of A is:"+a+"<br>");
+var b=+prompt("Enter B value:");
+document.write(" Value of B is:"+a+"<br>");
+var add=a+b; 
+document.write("Adding values="+add);
+
+
