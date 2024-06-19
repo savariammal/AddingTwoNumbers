44a5ef678aa03f3da2949f177b3dde27e8e1bb61
savariammal <160878030+savariammal@users.noreply.github.com>
2024-06-19 10:37:57 +0530

    Update add.js

--- add.js
+++ add.js
@@ -1,9 +1,11 @@
-
-var a=+prompt("Enter A value:");
-document.write(" Value of A is:"+a+"<br>");
-var b=+prompt("Enter B value:");
-document.write(" Value of B is:"+a+"<br>");
-var add=a+b; 
-document.write("Adding values="+add);
-
-
+function addnumber() {
+    var box1 = document.getElementById("num1");
+    var box2 = document.getElementById("num2");
+    var result = document.getElementById("result-value");
+    
+    var box1value = Number(box1.value);
+    var box2value = Number(box2.value);
+    var total = box1value + box2value;
+    
+    result.textContent = total;
+}
